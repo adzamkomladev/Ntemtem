24a0a11c96b52b87fd0172df702961c46fe1abab
preshlele <emomensa@gmail.com>
2021-01-13 20:43:51 +0800

    These are the properties you asked me to do. Pls check it out and give me feedback

---
+++ NtemtemApi/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Properties
+
+{
+	
+	public Class1()
+	{
+	public int Id { get; set }
+	public DateTime Start { get; set }
+	public string Purpose { get; set }
+	public string Location { get; set }
+	public DateTime End { get; set }
+	public string Status { get; set }
+	public DateTime CreatedAt { get; set }
+	public DateTime UpdatedAt { get; set }
+
+
+
+
+
+
+
+}
+}
