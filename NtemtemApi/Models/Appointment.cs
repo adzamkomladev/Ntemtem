3b4e0abc3a977bce2277bd4eaad4bb36137339aa
preshlele <emomensa@gmail.com>
2021-01-16 09:04:47 +0800

    Updated appointed models

--- NtemtemApi/Models/Appointment.cs
+++ NtemtemApi/Models/Appointment.cs
@@ -1,25 +1,32 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
-public class Properties
-
+namespace NtemtemApi.Models
 {
-	
-	public Class1()
-	{
-	public int Id { get; set }
-	public DateTime Start { get; set }
-	public string Purpose { get; set }
-	public string Location { get; set }
-	public DateTime End { get; set }
-	public string Status { get; set }
-	public DateTime CreatedAt { get; set }
-	public DateTime UpdatedAt { get; set }
+    public class Appointment
+    {
+        public int Id { get; set; }
 
+        [Required]
+        public DateTime Start { get; set; }
 
+        [Required]
+        public DateTime End { get; set; }
 
+        [Required]
+        public string Timezone { get; set; }
 
+        [Required]
+        public string Purpose { get; set; }
 
+        [Required]
+        public string Location { get; set; }
 
+        [Required]
+        public string Status { get; set; }
 
-}
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
 }
